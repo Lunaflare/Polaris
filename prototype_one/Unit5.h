08e69f829b453992f1b5d87c7e310e239816221d
alexfehr15 <alexfehr15@gmail.com>
2014-01-30 16:22:18 -0500

    Added settings options (no functionality yet)
    Added buttons, text, radio buttons, edit areas but did not yet add
    functionality

--- prototype_one/Unit5.h
+++ prototype_one/Unit5.h
@@ -7,11 +7,24 @@
 #include <FMX.Controls.hpp>
 #include <FMX.Forms.hpp>
 #include <FMX.Types.hpp>
+#include <FMX.Edit.hpp>
+#include <FMX.Effects.hpp>
 //---------------------------------------------------------------------------
 class TForm5 : public TForm
 {
 __published:	// IDE-managed Components
 	TStyleBook *StyleBook2;
+	TLabel *readModeLabel;
+	TLabel *InputModeLabel;
+	TLabel *passwordChangeLabel;
+	TRadioButton *inputDefaultRadio;
+	TRadioButton *inputAdvancedRadio;
+	TRadioButton *readDefaultRadio;
+	TRadioButton *readAdvancedRadio;
+	TEdit *passwordChangeEdit;
+	TGlowEffect *passwordGlowEffect;
+	TButton *changePasswordButton;
+	TButton *editDLRButton;
 	void __fastcall FormShow(TObject *Sender);
 	void __fastcall FormClose(TObject *Sender, TCloseAction &Action);
 private:	// User declarations
